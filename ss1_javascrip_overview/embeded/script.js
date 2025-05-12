00fac5a73c60c7e4fbf068a72bd7362d1d2b167d
c0623c1 <c0623c1forgit@mail1s.edu.vn>
2025-05-12 23:10:53 +0700

    ss1 - vuejs overview

---
+++ ss1_javascrip_overview/embeded/script.js
@@ -0,0 +1,13 @@
+function changeData() {
+    document.getElementById("content").innerText = "Xin chào!!!";
+    document.getElementById("content").style.color = "red";
+    document.getElementById("content").style.fontSize = "20px";
+    document.getElementById("content").style.fontWeight = "bold";
+    document.getElementById("content").style.textAlign = "center";
+    document.getElementById("content").style.backgroundColor = "blue";
+    document.getElementById("content").style.padding = "10px";
+    document.getElementById("content").style.borderRadius = "10px";
+    document.getElementById("content").style.boxShadow =
+      "0 0 10px 0 rgba(0, 0, 0, 0.5)";
+    document.getElementById("content").style.transition = "all 0.3s ease";
+  }
